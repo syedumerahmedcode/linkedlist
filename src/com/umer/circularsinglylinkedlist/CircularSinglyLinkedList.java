346dac04d99fb8a0b3b1de6160588c3737c278cf
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-02-26 13:53:54 +0100

    Feature: Implemented the method which creates the circular singly linked list.

--- src/com/umer/circularsinglylinkedlist/CircularSinglyLinkedList.java
+++ src/com/umer/circularsinglylinkedlist/CircularSinglyLinkedList.java
@@ -1,5 +1,22 @@
 package com.umer.circularsinglylinkedlist;
 
+import com.umer.common.Node;
+
 public class CircularSinglyLinkedList {
+	public Node head;
+	public Node tail;
+	public int size;
+
+	public Node CreateCircularSinglyLinkedList(int nodeValue) {
+		head = new Node();
+		Node node = new Node();
+		node.value = nodeValue;
+		// This makes the initial list circular since the head is pointing to itself.
+		node.next = node;
+		head = node;
+		tail = node;
+		size = 1;
+		return head;
+	}
 
 }
