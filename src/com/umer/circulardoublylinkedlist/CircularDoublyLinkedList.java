4439afea59ab40c08e1efdd003e47bcbbb7bedd5
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-05-29 12:46:12 +0200

    Feature: Implemented the delete() method for circular double linked list.

--- src/com/umer/circulardoublylinkedlist/CircularDoublyLinkedList.java
+++ src/com/umer/circulardoublylinkedlist/CircularDoublyLinkedList.java
@@ -107,5 +107,55 @@ public class CircularDoublyLinkedList {
 		System.out.println("The value provided: "+nodeValue+" does not exist.");
 		return false;
 	}
+	
+	// deletion method
+	public void deleteNode(int location) {
+		if(head==null) {
+			System.out.println("The cicular double linked list does not exist");
+			return;			
+		}
+		else if(location==0) {
+			// We are deleting a node at the start of the circular double linked list
+			if(size==1) {
+				// The circular double linked list contains only one element
+				head.prev=null;
+				head.next=null;
+				head=tail=null;
+				size--;
+				return;				
+			}else {
+				// The circular double linked list contains more than one element
+				head= head.next;
+				head.prev=tail;
+				tail.next=head;
+				size--;
+			}
+		}else if(location>=size) {
+			// We are deleting a node at the end of the circular double linked list
+			if(size==1) {
+				// The circular double linked list contains only one element
+				head.prev=null;
+				head.next=null;
+				head=tail=null;
+				size--;
+				return;				
+			}else {
+				// The circular double linked list contains more than one element
+				tail=tail.prev;
+				tail.next=head;
+				head.prev=tail;
+				size--;
+			}
+		}else {
+			// We are deleting a node at any given location of the circular double linked list
+			DoublyNode tempNode=head;
+			for(int i=0;i<location-1;i++) {
+				tempNode=tempNode.next;
+			}
+			tempNode.next=tempNode.next.next;
+			tempNode.next.prev=tempNode;
+			size--;
+		}
+	}
 
 }
